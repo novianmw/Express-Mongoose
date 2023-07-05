b345643d5ee94c6f54321b3fde420dbc971ad21d
novianmw <novian.mulyoadi.w@gmail.com>
2023-07-05 12:45:48 +0700

    add folder models

---
+++ models/product.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    }, 
+    brand: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    size: {
+        type: String,
+        enum: ['S', 'M', 'L', 'XL'],
+    }
+})
+
+const Product = mongoose.model('Product', productSchema)
+module.exports = Product
