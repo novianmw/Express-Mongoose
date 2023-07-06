706895b8745447dc1782b56ac50164c6583cd738
novianmw <novian.mulyoadi.w@gmail.com>
2023-07-06 14:42:06 +0700

    update seed product

--- seed.js
+++ seed.js
@@ -9,45 +9,73 @@ mongoose.connect('mongodb://127.0.0.1/shop_db')
         console.log('MongoDB connection failed', error)
     })
 
-const seedProduct = [
-    {
-        "name": "Kemeja Flanel",
-        "brand": "Hollister",
-        "price": 750000,
-        "color": "biru muda",
-        "size": "M",
-    },
-    {
-        "name": "Sweater",
-        "brand": "Gap",
-        "price": 650000,
-        "color": "merah muda",
-        "size": "XL"
-    },
-    {
-        "name": "Baju Renang",
-        "brand": "Speedo",
-        "price": 500000,
-        "color": "biru tua",
-        "size": "S"
-    },
-    {
-        "name": "Rompi",
-        "brand": "Zara",
-        "price": 850000,
-        "color": "abu-abu",
-        "size": "L"
-    },
-    {
-        "name": "Jas",
-        "brand": "Hugo Boss",
-        "price": 4500000,
-        "color": "hitam",
-        "size": "XL"
-    }
-]
+    const seedProducts = [
+        {
+            "name": "Kemeja Flanel",
+            "brand": "Hollister",
+            "price": 750000,
+            "color": "biru muda",
+            "category": "Baju",
+        },
+        {
+            "name": "Celana Chino",
+            "brand": "Levi's",
+            "price": 900000,
+            "color": "krem",
+            "category": "Celana",
+        },
+        {
+            "name": "Sweater",
+            "brand": "Gap",
+            "price": 650000,
+            "color": "merah muda",
+            "category": "Jaket",
+        },
+        {
+            "name": "Tas Ransel",
+            "brand": "Herschel",
+            "price": 1500000,
+            "color": "biru",
+            "category": "Aksesoris",
+        },
+        {
+            "name": "Kacamata Aviator",
+            "brand": "Ray-Ban",
+            "price": 2000000,
+            "color": "emas",
+            "category": "Aksesoris",
+        },
+        {
+            "name": "Baju Renang",
+            "brand": "Speedo",
+            "price": 500000,
+            "color": "biru tua",
+            "category": "Baju",
+        },
+        {
+            "name": "Topi Baseball",
+            "brand": "New Era",
+            "price": 350000,
+            "color": "hitam",
+            "category": "Aksesoris",
+        },
+        {
+            "name": "Rompi",
+            "brand": "Zara",
+            "price": 850000,
+            "color": "abu-abu",
+            "category": "Aksesoris",
+        },
+        {
+            "name": "Jas",
+            "brand": "Hugo Boss",
+            "price": 4500000,
+            "color": "hitam",
+            "category": "Baju",
+        },
+    ]
 
-Product.insertMany(seedProduct).then((result) => {
+Product.insertMany(seedProducts).then((result) => {
     console.log('seed product success',result)
 }).catch((error) => {
     console.log('seed product failed', error)
