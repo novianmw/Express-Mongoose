129b594205764fd9d8b1f5155d37aec90fe6b3f7
novianmw <novian.mulyoadi.w@gmail.com>
2023-07-14 07:37:53 +0700

    add module garment

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const ErrorHandler = require('./utils/ErrorHandler')
 
 // models
 const Product = require('./models/product')
+const Garment = require('./models/garment')
 
 // connect to mongodb
 mongoose.connect('mongodb://127.0.0.1/shop_db')
@@ -21,18 +22,26 @@ app.set('view engine', 'ejs') // file ejs
 app.use(express.urlencoded({ extended: true })) // untuk membaca data dari form
 app.use(methodOverride('_method')) // untuk mengubah method POST atau GET menjadi Update atau DELETE
 
-// pengganti try catch
+// middleware pengganti try catch 
 function wrapAsync(fn) { 
     return function(req, res, next) {
         fn(req, res, next).catch(err => next(err))
     }
 }
 
+
 // OPENING (landing page)
 app.get('/', (req, res) => {
-    res.send('<h1>Hello World</h1>' + '<h2>Silahkan ke menu di bawah ini</h2>' + '<a href="/products">Products</a>')
+    res.send('<h1>Hello World</h1>' + '<h2>Silahkan ke menu di bawah ini</h2>' + '<a href="/products">Products</a> ' + '<a href="/garments">garments</a>')
+    
 })
 
+
+// GET list GARMENT
+app.get('/garments', wrapAsync(async (req,res) => {
+    const garments = await Garment.find({})
+    res.render('garments/index', { garments })
+}))
 // GET list PRODUCTS
 app.get('/products', async (req, res) => {
     const { category } = req.query
@@ -45,30 +54,85 @@ app.get('/products', async (req, res) => {
     }
 })
 
-// Get form for create new product
+
+// GET form for create new garment
+app.get('/garments/create', (req, res) => {
+    res.render('garments/create')
+})
+// GET form for create new product
 app.get('/products/create', (req, res) => {
     res.render('products/create')
 })
 
-// POST create new product from form
+
+// GET form for create new product in garment
+// /garments/:garment_id/product/create
+app.get('/garments/:garment_id/products/create', (req, res) => {
+    const { garment_id } = req.params
+    res.render('products/create', { garment_id })
+})
+
+// POST create new product in garment
+// /garments/:garment_id/products
+app.post('/garments/:garment_id/products', wrapAsync(async (req, res) => {
+    const { garment_id } = req.params
+    const garment = await Garment.findById(garment_id)
+    const product = new Product(req.body)
+    garment.products.push(product)
+    product.garment = garment
+    await garment.save()
+    await product.save()
+    res.redirect(`/garments/${garment_id}`)
+}))
+
+
+
+
+
+// POST create new garment from form create
+app.post('/garments', wrapAsync(async (req, res) => {
+    const garment = new Garment(req.body)
+    await garment.save()
+    res.redirect(`/garments`)
+}))
+// POST create new product from form create
 app.post('/products', wrapAsync(async (req, res) => {
     const product = new Product(req.body)
     await product.save()
     res.redirect(`/products/${product._id}`)
 }))
 
+// GET detail garment berdasarkan ID garment
+app.get('/garments/:id', wrapAsync(async (req, res) => {
+    const { id } = req.params
+    const garment = await Garment.findById(id).populate('products') // populate untuk mengambil data dari product
+    res.render('garments/show', { garment })
+}))
 // GET detail product berdasarkan ID product
 app.get('/products/:id', wrapAsync(async (req, res) => {
         const { id } = req.params
-        const product = await Product.findById(id)
+        const product = await Product.findById(id).populate('garment')
         res.render('products/show', { product })            
 }))
 
+// get detail garment di form edit
+app.get('/garments/:id/edit', wrapAsync(async (req, res) => {
+    const { id } = req.params
+    const garment = await Garment.findById(id)
+    res.render('garments/edit', { garment })
+}))
 // Get detail product di form edit
 app.get('/products/:id/edit', wrapAsync(async (req, res) => {
         const { id } = req.params
         const product = await Product.findById(id)
         res.render('products/edit', { product })
+}))
+
+// Update garment yang di pilih form edit
+app.put('/garments/:id', wrapAsync(async (req, res) => {
+    const { id } = req.params
+    const garment = await Garment.findByIdAndUpdate(id, req.body, { runValidators: true })
+    res.redirect(`/garments/${garment._id}`)
 }))
 
 // Update product yang di pilih form edit
@@ -78,6 +142,12 @@ app.put('/products/:id', wrapAsync(async (req, res) => {
         res.redirect(`/products/${product._id}`)
 }))
 
+// Delete garment yang di pilih
+app.delete('/garments/:garment_id', wrapAsync(async (req, res) => {
+    const { garment_id } = req.params
+    await Garment.findOneAndDelete({_id: garment_id})
+    res.redirect('/garments')
+}))
 // Delete product yang di pilih
 app.delete('/products/:id', wrapAsync(async (req, res) => {
         const { id } = req.params
@@ -102,6 +172,7 @@ app.use((err, req, res, next) => {
     next(err)
 })
 
+// error router wrong path
 app.use((err, req, res, next) => {
     const { status = 500, message = 'Something went wrong !' } = err
     res.status(status).send(message)
